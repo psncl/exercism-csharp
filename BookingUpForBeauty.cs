c609542b8855c1912b6b0249fd6a5d4f850a59d5
psncl <ps@ps.ncl>
2025-03-24 17:06:40 +0000

    Add DateTime exercise

---
+++ BookingUpForBeauty.cs
@@ -0,0 +1,27 @@
+static class Appointment
+{
+    public static DateTime Schedule(string appointmentDateDescription)
+    {
+        return DateTime.Parse(appointmentDateDescription);
+    }
+
+    public static bool HasPassed(DateTime appointmentDate)
+    {
+        return appointmentDate < DateTime.Now;
+    }
+
+    public static bool IsAfternoonAppointment(DateTime appointmentDate)
+    {
+        return appointmentDate.Hour is >= 12 and < 18;
+    }
+
+    public static string Description(DateTime appointmentDate)
+    {
+        return $"You have an appointment on {DateTime.Parse(appointmentDate.ToString("G"))}.";
+    }
+
+    public static DateTime AnniversaryDate()
+    {
+        return new DateTime(DateTime.Now.Year, 9, 15);
+    }
+}
