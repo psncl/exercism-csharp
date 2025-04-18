dc4f252fe08dfbd261d085a53bd1eef878ab61b6
psncl <pushkarsinhaw@gmail.com>
2025-04-18 14:46:49 +0530

    Add exercise 14

---
+++ 14. CalculatorConundrum.cs
@@ -0,0 +1,34 @@
+public static class SimpleCalculator
+{
+    public static string Calculate(int operand1, int operand2, string? operation)
+    {
+        double result;
+        
+        switch (operation)
+        {
+            case "+":
+                result = SimpleOperation.Addition(operand1, operand2);
+                break;
+            case "*":
+                result = SimpleOperation.Multiplication(operand1, operand2);
+                break;
+            case "/":
+                try
+                {
+                    result = SimpleOperation.Division(operand1, operand2);
+                    break;
+                }
+                catch (DivideByZeroException)
+                {
+                    return "Division by zero is not allowed.";
+                }
+            case "":
+                throw new ArgumentException();
+            case null:
+                throw new ArgumentNullException();
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        return $"{operand1} {operation} {operand2} = {result}";
+    }
+}
