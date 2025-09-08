f9933f93a8bd17cbe21dd478ec5cb7eeee7b7731
psncl <pushkarsinhaw@gmail.com>
2025-09-08 22:30:34 +0100

    Add exercise 49. HighScores

---
+++ 49. HighScores.cs
@@ -0,0 +1,17 @@
+public class HighScores
+{
+    private List<int> _scores;
+    
+    public HighScores(List<int> list)
+    {
+        _scores = list;
+    }
+
+    public List<int> Scores() => _scores;
+
+    public int Latest() => _scores.Last();
+
+    public int PersonalBest() => _scores.Max();
+
+    public List<int> PersonalTopThree() => _scores.OrderDescending().Take(3).ToList();
+}
