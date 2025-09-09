34ec4b7b620ff4feea44ac6b175d98142e4ff54b
psncl <pushkarsinhaw@gmail.com>
2025-09-09 18:34:21 +0100

    Add exercise 21. TracksOnTracksOnTracks

---
+++ 21. TracksOnTracksOnTracks.cs
@@ -0,0 +1,46 @@
+public static class Languages
+{
+    public static List<string> NewList()
+    {
+        return new List<string>();
+    }
+
+    public static List<string> GetExistingLanguages()
+    {
+        return new List<string> { "C#", "Clojure", "Elm" };
+    }
+
+    public static List<string> AddLanguage(List<string> languages, string language)
+    {
+        languages.Add(language);
+        return languages;
+    }
+
+    public static int CountLanguages(List<string> languages) => languages.Count;
+
+    public static bool HasLanguage(List<string> languages, string language) => languages.Contains(language);
+
+    public static List<string> ReverseList(List<string> languages)
+    {
+        languages.Reverse();
+        return languages;
+    }
+
+    public static bool IsExciting(List<string> languages)
+    {
+        if (CountLanguages(languages) == 0) return false;
+        
+        const string favoriteLanguage = "C#";
+        if (languages.First() == favoriteLanguage) return true;
+        if (CountLanguages(languages) is 2 or 3) return languages[1] == favoriteLanguage;
+        return false;
+    }
+
+    public static List<string> RemoveLanguage(List<string> languages, string language)
+    {
+        languages.Remove(language);
+        return languages;
+    }
+
+    public static bool IsUnique(List<string> languages) => languages.Count == languages.Distinct().Count();
+}
