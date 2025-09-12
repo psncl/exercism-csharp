0d58ef3f359211b0302d5506cf54771b17ba95cf
psncl <pushkarsinhaw@gmail.com>
2025-09-12 12:17:33 +0100

    Add 28. HyperiaForex

---
+++ 28. HyperiaForex.cs
@@ -0,0 +1,69 @@
+public struct CurrencyAmount
+{
+    private decimal amount;
+    private string currency;
+
+    public CurrencyAmount(decimal amount, string currency)
+    {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    public static bool operator ==(CurrencyAmount left, CurrencyAmount right)
+    {
+        IsNotSameCurrency(left, right);
+        return left.amount == right.amount;
+    }
+    
+    public static bool operator !=(CurrencyAmount left, CurrencyAmount right) => !(left == right);
+    
+    public static bool operator >(CurrencyAmount left, CurrencyAmount right)
+    {
+        IsNotSameCurrency(left, right);
+        return left.amount > right.amount;
+    }
+    
+    public static bool operator <(CurrencyAmount left, CurrencyAmount right)
+    {
+        IsNotSameCurrency(left, right);
+        return left.amount < right.amount;
+    }
+
+    public static CurrencyAmount operator +(CurrencyAmount left, CurrencyAmount right)
+    {
+        IsNotSameCurrency(left, right);
+        return left with { amount = left.amount + right.amount };
+    }
+    
+    public static CurrencyAmount operator -(CurrencyAmount left, CurrencyAmount right)
+    {
+        IsNotSameCurrency(left, right);
+        return left with { amount = left.amount - right.amount };
+    }
+    
+    public static CurrencyAmount operator *(CurrencyAmount currency, decimal factor)
+    {
+        return currency with { amount = currency.amount * factor };
+    }
+    
+    public static CurrencyAmount operator *(decimal factor, CurrencyAmount currency)
+    {
+        return currency with { amount = factor * currency.amount };
+    }
+    
+    public static CurrencyAmount operator /(CurrencyAmount currency, decimal divisor)
+    {
+        if (divisor == 0) throw new ArgumentException();
+        return currency with { amount = currency.amount / divisor };
+    }
+    
+    public static explicit operator double(CurrencyAmount currency) => (double)currency.amount;
+    
+    public static implicit operator decimal(CurrencyAmount currency) => currency.amount;
+
+    private static void IsNotSameCurrency(CurrencyAmount left, CurrencyAmount right)
+    {
+        if (left.currency != right.currency) throw new ArgumentException();
+    }
+        
+}
