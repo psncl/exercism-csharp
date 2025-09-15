6804d677204457cf43a55bce545b0970f345045d
psncl <pushkarsinhaw@gmail.com>
2025-09-15 23:22:28 +0100

    Add 31. WeighingMachine

---
+++ 31. WeighingMachine.cs
@@ -0,0 +1,20 @@
+class WeighingMachine(int precision)
+{
+    public int Precision { get; } = precision;
+
+    private double _weight;
+    
+    public double Weight
+    {
+        get => _weight;
+        set
+        {
+            if (value < 0) throw new ArgumentOutOfRangeException();
+            _weight = value;
+        }
+    }
+
+    public double TareAdjustment { get; set; } = 5.0d;
+
+    public string DisplayWeight => $"{(Weight - TareAdjustment).ToString($"F{Precision}")} kg";
+}
